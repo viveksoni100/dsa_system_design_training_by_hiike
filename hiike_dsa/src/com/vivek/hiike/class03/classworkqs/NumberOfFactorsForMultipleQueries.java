bbac4b3b2174cf7a6b49b67f4e2f53e2f2618bc4
viveksoni100 <viveksoni100@gmail.com>
2024-05-09 00:13:12 +0530

    class 04 notes added

---
+++ hiike_dsa/src/com/vivek/hiike/class03/classworkqs/NumberOfFactorsForMultipleQueries.java
@@ -0,0 +1,45 @@
+package com.vivek.hiike.class03.classworkqs;
+
+/**
+ * 3.Number of factors for multiple queries
+ * https://www.hackerrank.com/contests/class-3-inclass-questions/challenges/number-of-factors-for-multiple-queries
+ *
+ * Given Q numbers, for every number N find the number of factors.
+ *
+ * Input Format
+ *
+ *     The first line contains an integer Q denoting the number of queries.
+ *     Each of the subsequent Q lines contain an integer N.
+ *
+ * Constraints
+ *
+ *     Q>0
+ *     1 < N < 10000
+ *
+ * Output Format
+ *
+ * Print number of factors of each query separated by a space.
+ *
+ * Sample Input 0
+ *
+ * 10
+ * 45 678 2345 765 123 5656 9 1 12 889
+ *
+ * Sample Output 0
+ *
+ * 6 8 8 12 4 16 3 1 6 4
+ *
+ * Sample Input 1
+ *
+ * 5
+ * 10 2 14 8 3
+ *
+ * Sample Output 1
+ *
+ * 4 2 4 4 2
+ */
+public class NumberOfFactorsForMultipleQueries {
+    public static void main(String[] args) {
+
+    }
+}
